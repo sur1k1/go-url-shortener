946ae6eeba897abcd2d04166bc82ee3809de552b
sparr0w666 <sparr0w666@proton.me>
2025-03-12 11:27:05 +0700

    add: parse flag log level, check env log level

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,6 +8,7 @@ import (
 type Config struct {
 	ServerAddress string
 	BaseURL 			string
+	LogLevel			string
 }
 
 func MustGetConfig() *Config {
@@ -15,6 +16,7 @@ func MustGetConfig() *Config {
 
 	flag.StringVar(&c.ServerAddress, "a", "localhost:8080", "server start address")
 	flag.StringVar(&c.BaseURL, "b", "http://localhost:8080", "server address before the short URL")
+	flag.StringVar(&c.LogLevel, "l", "info", "level logging")
 
 	flag.Parse()
 
@@ -22,6 +24,9 @@ func MustGetConfig() *Config {
 		c.ServerAddress = servAddress
 	}
 	if baseURL, ok := os.LookupEnv("BASE_URL"); ok {
+		c.BaseURL = baseURL
+	}
+	if baseURL, ok := os.LookupEnv("LOG_LEVEL"); ok {
 		c.BaseURL = baseURL
 	}
 
